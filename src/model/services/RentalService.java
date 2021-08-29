a0eac375a14581ad53df8df5c2db75ebe75b0e51
augusto <gutim160@gmail.com>
2021-08-29 08:49:55 -0300

    Solução com interface

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -8,13 +8,13 @@ public class RentalService {
 	private Double pricePerDay;
 	private Double pricePerHour;
 	
-	private BrazilTaxService taxService;
+	private TaxService taxService;
 	
 	public RentalService() {
 
 	}
 
-	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+	public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
 		this.pricePerDay = pricePerDay;
 		this.pricePerHour = pricePerHour;
 		this.taxService = taxService;
@@ -36,7 +36,7 @@ public class RentalService {
 		this.pricePerHour = pricePerHour;
 	}
 
-	public BrazilTaxService getTaxService() {
+	public TaxService getTaxService() {
 		return taxService;
 	}
 
