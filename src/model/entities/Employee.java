914e7fdab83d3a01c571ea9315129412573743d5
augusto <gutim160@gmail.com>
2021-09-01 11:13:43 -0300

    Comparable implements

---
+++ src/model/entities/Employee.java
@@ -0,0 +1,38 @@
+package model.entities;
+
+public class Employee implements Comparable<Employee>{
+	
+	private String nome;
+	private Double salary;
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(String nome, Double salary) {
+		super();
+		this.nome = nome;
+		this.salary = salary;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public int compareTo(Employee other) {
+		return -this.salary.compareTo(other.getSalary());
+	}
+}
